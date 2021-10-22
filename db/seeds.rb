7fd261deb92cc36e2755fdd9a93323fde6815645
eduardo pimenta <ed83isme@gmail.com>
2021-10-22 16:06:34 -0300

    added db schema, models and db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+buffer = URI.open(url).read
+top_movies = JSON.parse(buffer)["results"]
+
+puts "Deleting database..."
+Movie.destroy_all
+puts "Database is empty."
+
+puts "Creating new movies..."
+top_movies.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+puts "#{Movie.count} movies were added."
